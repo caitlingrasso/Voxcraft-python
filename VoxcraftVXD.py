e56cefdf0ca0382db58e71c3d1fd480cf51e8038
Caitlin Grasso <caitlingrasso@Caitlins-MacBook-Pro-2.local>
2021-08-18 15:09:31 -0400

    Added record history tags

--- VoxcraftVXD.py
+++ VoxcraftVXD.py
@@ -8,6 +8,22 @@ class VXD:
         root = etree.XML("<VXD></VXD>")
         self.tree = etree.ElementTree(root)
 
+
+    
+    def set_vxd_tags(self, RecordVoxel=1, RecordLink=0, RecordFixedVoxels=1, RecordStepSize=100):
+        root = self.tree.getroot()
+
+        history = etree.SubElement(root, "RecordHistory")
+        history.set('replace', 'VXA.Simulator.RecordHistory')
+        etree.SubElement(history, "RecordStepSize").text = str(RecordStepSize)
+        etree.SubElement(history, "RecordVoxel").text = str(RecordVoxel)
+        etree.SubElement(history, "RecordLink").text = str(RecordLink) 
+        etree.SubElement(history, "RecordFixedVoxels").text = str(RecordFixedVoxels) 
+
+    def set_data(self, data):
+        #TODO: takes 3D np array and sets data by overwriting the VXA
+        pass
+
     def write(self, filename='robot.vxd'):
         os.makedirs('data', exist_ok=True)
         
