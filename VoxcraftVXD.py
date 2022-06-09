7de3157169f50d8a8c4bd0a3470ca2419d74acb8
Federico Pigozzi <pigozzife@gmail.com>
2022-06-08 21:58:44 -0400

    fix bug when writing data to .vxd
    use x-wise instead of y-wise

--- VoxcraftVXD.py
+++ VoxcraftVXD.py
@@ -23,8 +23,12 @@ class VXD:
 
         X_Voxels, Y_Voxels, Z_Voxels  = data.shape
         body_flatten = np.zeros((X_Voxels*Y_Voxels, Z_Voxels),dtype=np.int8)
-        for i in range(Z_Voxels):
-            body_flatten[:,i] = data[:,:,i].flatten()
+        for z in range(Z_Voxels):
+            k = 0
+            for y in range(Y_Voxels):
+                for x in range(X_Voxels):
+                    body_flatten[k, z] = data[x, y, z]
+                    k += 1
         
         structure = etree.SubElement(root, "Structure")
         structure.set('replace', 'VXA.VXC.Structure')
